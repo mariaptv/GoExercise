9ec498666a1f7c73a6a065554bfcf0b176019957
maria.tellez <maria.tellez@sezzle.com>
2025-02-11 20:46:33 -0500

    exercises

--- exercise_twenty.go
+++ exercise_twenty.go
@@ -5,11 +5,7 @@ import (
 	"math/rand"
 )
 
-func init() {
-	fmt.Println("Welcome to exercise_twenty_one!")
-}
-
-func main() {
+func mainExerciseTwenty() {
 	randomNumber := rand.Intn(250) // Random number between 0 and 99
 	fmt.Println("Random number:", randomNumber)
 	fmt.Printf("The variable 'randomNumber' is %d\n", randomNumber)
@@ -18,15 +14,6 @@ func main() {
 	} else if randomNumber > 101 && randomNumber < 200 {
 		fmt.Println("The random number is between 101 and 200")
 	} else {
-		fmt.Println("The random number is between 201 and 250")
-	}
-
-	switch {
-	case randomNumber > 0 && randomNumber < 100:
-		fmt.Println("The random number is between 0 and 100")
-	case randomNumber > 101 && randomNumber < 200:
-		fmt.Println("The random number is between 101 and 200")
-	case randomNumber > 201 && randomNumber < 250:
 		fmt.Println("The random number is between 201 and 250")
 	}
 }
